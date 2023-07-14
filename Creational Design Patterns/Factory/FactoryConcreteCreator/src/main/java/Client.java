4069050c7ed47bbb42596e4a6e2c7e2c781733bf
Mahmoud Mehisen <mahmoud.mehisen@gmail.com>
2023-07-14 15:07:03 +0300

    modfiy concret creator

---
+++ Creational Design Patterns/Factory/FactoryConcreteCreator/src/main/java/Client.java
@@ -0,0 +1,12 @@
+public class Client {
+    public static void main(String[] args){
+        Shape shape1 = new ShapeFactory().getShape("CIRCLE");
+        shape1.draw();
+
+        Shape shape2 = new ShapeFactory().getShape("RECTANGLE");
+        shape2.draw();
+
+        Shape shape3 = new ShapeFactory().getShape("SQUARE");
+        shape3.draw();
+    }
+}
